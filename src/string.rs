889a1a7c42e6c4f08cf3b78ef029e11822f81363
Wolf McNally <wolf@wolfmcnally.com>
2023-01-19 15:29:48 -0800

    Working.

--- src/string.rs
+++ src/string.rs
@@ -1,10 +1,24 @@
 use crate::{cbor::CBOREncode, varint::VarIntEncode};
 
-impl CBOREncode for str {
+impl CBOREncode for &str {
     fn cbor_encode(&self) -> Vec<u8> {
         let mut data: Vec<u8> = self.bytes().collect();
         let mut buf = data.len().varint_encode(3);
         buf.append(&mut data);
         buf
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{hex::bytes_to_hex, cbor::CBOREncode, bytestring::ByteString};
+
+    fn test_encode<T: CBOREncode>(t: T, expected: &str) {
+        assert_eq!(bytes_to_hex(&t.cbor_encode()), expected);
+    }
+
+    #[test]
+    fn encode_bytestring() {
+        test_encode("Hello", "43112233");
     }
 }
