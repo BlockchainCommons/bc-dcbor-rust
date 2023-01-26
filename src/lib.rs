8093add3a3aef2ba6cb02b584b9967381298d1eb
Wolf McNally <wolf@wolfmcnally.com>
2023-01-25 23:35:10 -0800

    Working.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,18 @@
-mod util;
-
 pub mod cbor;
 
 #[cfg(test)]
 mod test {
-    use crate::{cbor::{Tagged, CBOREncodable, decode, bytes_to_hex, Bytes, Map, hex_to_bytes, DecodeError, Value}, util::test_util::test_cbor};
+    use crate::{cbor::{Tagged, CBOREncodable, decode, bytes_to_hex, Bytes, Map, hex_to_bytes, DecodeError, Value}};
+
+    fn test_cbor<T>(t: T, expected_debug: &str, expected_display: &str, expected_data: &str) where T: CBOREncodable {
+        let cbor = t.cbor();
+        assert_eq!(format!("{:?}", cbor), expected_debug);
+        assert_eq!(format!("{}", cbor), expected_display);
+        let data = cbor.encode_cbor();
+        assert_eq!(bytes_to_hex(&data), expected_data);
+        let decoded_cbor = decode(&data).unwrap();
+        assert_eq!(cbor, decoded_cbor);
+    }
 
     #[test]
     fn encode_unsigned() {
