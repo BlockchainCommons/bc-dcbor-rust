067734f918c9cf29d357c4a3d90156d695fd6af9
Wolf McNally <wolf@wolfmcnally.com>
2023-09-20 19:46:39 -0700

    Convenience for CBORTaggedEncodable.

--- src/cbor_tagged_encodable.rs
+++ src/cbor_tagged_encodable.rs
@@ -13,4 +13,9 @@ pub trait CBORTaggedEncodable: CBOREncodable + CBORTagged {
     fn tagged_cbor(&self) -> CBOR {
         CBOR::Tagged(Self::CBOR_TAG, Box::new(self.untagged_cbor()))
     }
+
+    /// Returns the tagged value in CBOR binary representation.
+    fn tagged_cbor_data(&self) -> Vec<u8> {
+        self.tagged_cbor().cbor_data()
+    }
 }
